1411c1d1b16ceba9fcbd518b8a2f9d7036fe3019
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-06 01:25:00 +0530

    program to find contact details added

---
+++ src/com/yml/address/Contact.java
@@ -0,0 +1,29 @@
+package com.yml.address;
+
+public class Contact {
+	private String firstName,lastName,address,city,state,zip,phoneNumber,email;
+	public Contact(String firstName,String lastName,String address,String city,String state,String zip,String phoneNumber,String email)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
+		this.email=email;
+	}
+	
+	public void display()
+	{
+		System.out.println("First Name:"+firstName);
+		System.out.println("Last Name:"+lastName);
+		System.out.println("Address:"+address);
+		System.out.println("City:"+city);
+		System.out.println("State:"+state);
+		System.out.println("Zip:"+zip);
+		System.out.println("Phone number"+phoneNumber);
+		System.out.println("E-mail:"+email);
+	}
+}
+
